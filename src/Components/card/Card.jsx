af37c25fec68888d88c47bfd404565557bc20786
manoelteixeira <manoel.teixeira.cc@gmail.com>
2024-04-17 13:18:06 -0400

    Fix image linking

--- src/Components/card/Card.jsx
+++ src/Components/card/Card.jsx
@@ -1,10 +1,12 @@
 // import React from 'react'
 import "./card.scss"
+import person from "../../assets/person.jpg"
 
+// eslint-disable-next-line react/prop-types
 const Card = ({name}) => {
   return (
     <div className="card">
-      <img src="./src/assets/person.jpg" alt="" className="card__image" />
+      <img src={person} alt={name} className="card__image" />
       <span className="card__name">{name}</span>
     </div>
   )
--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 // import React from 'react'
 
 import "./navbar.scss"
+import flag from "../../assets/image.png"
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img src="./src/assets/image.png" alt="American Flag" className="navbar__image" />
+      <img src={flag} alt="American Flag" className="navbar__image" />
     </div>
   )
 }
